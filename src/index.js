d9aab9b408ef6996731ab7d3a10f2645b0404f24
Lakshmiprasad <slakshmiprasad93@gmail.com>
2018-02-06 01:22:10 +0530

    style: add material theme
    Use MuiThemeProvider
    create a theme using createMuiTheme

--- src/index.js
+++ src/index.js
@@ -6,12 +6,30 @@ import registerServiceWorker from './registerServiceWorker';
 import {BrowserRouter} from 'react-router-dom';
 import 'typeface-roboto';
 import Reboot from 'material-ui/Reboot';
-import Button from 'material-ui/Button';
+import { createMuiTheme } from 'material-ui/styles';
+import { MuiThemeProvider } from 'material-ui/styles';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#48a697',
+      main: '#00796b',
+      dark: '#004c40',
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: '#ffc947',
+      main: '#ff9800',
+      dark: '#c66900',
+      contrastText: '#000',
+    },
+  },
+});
 
 ReactDOM.render(
     <BrowserRouter>
         <Reboot>
-            <App />
+            <MuiThemeProvider theme={theme}><App/></MuiThemeProvider>
         </Reboot>
     </BrowserRouter>,
     document.getElementById('root'));
