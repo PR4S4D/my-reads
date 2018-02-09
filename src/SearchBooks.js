d8343fa17d6b267c6f166e3c0118e04b0196095c
Lakshmiprasad <slakshmiprasad93@gmail.com>
2018-02-10 01:15:49 +0530

    feat: show error message when there's no result + use debounce for input

--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -3,75 +3,81 @@ import { Component } from 'react';
 import * as BooksAPI from './BooksAPI';
 import DisplayBooks from './DisplayBooks';
 import { Link } from 'react-router-dom';
+import Typography from 'material-ui/Typography';
+import { DebounceInput } from 'react-debounce-input';
 
 class SearchBooks extends Component {
-  state = {
-    query: '',
-    searchResultsFound: false,
-    searchResults: null
-  };
+	state = {
+		query: '',
+		searchResultsFound: false,
+		searchResults: null
+	};
 
-  onSearchQueryChange = event => {
-    this.setState({
-      query: event.target.value
-    }); 
-  };
+	onSearchQueryChange = event => {
+		this.setState({
+			query: event.target.value
+		});
+	};
 
-  searchBooks = event => {
-    event.preventDefault();
-    BooksAPI.search(this.state.query).then(books => {
-      if (books && books.length > 0) {
-        if (this.props.currentBooks) {
-          this.props.currentBooks.forEach(element => {
-            let i = books.findIndex(b => element.id === b.id);
-            books[i] = element;
-          });
-        }
+	searchBooks = event => {
+		event.preventDefault();
+		BooksAPI.search(this.state.query).then(books => {
+			if (books && books.length > 0) {
+				if (this.props.currentBooks) {
+					this.props.currentBooks.forEach(element => {
+						let i = books.findIndex(b => element.id === b.id);
+						books[i] = element;
+					});
+				}
 
-        this.setState({
-          searchResultsFound: true,
-          searchResults: books
-        });
-      }
-    });
-  };
+				this.setState({
+					searchResultsFound: true,
+					searchResults: books
+				});
+			} else {
+				this.setState({ searchResultsFound: false });
+			}
+		});
+	};
 
-  render() {
-    return (
-      <div className="search-books">
-        <div className="search-books-bar">
-          <Link to="/" className="close-search" />
-          <div className="search-books-input-wrapper">
-            <form
-              onChange={this.searchBooks}
-              onSubmit={e => e.preventDefault()}
-            >
-              <input
-                type="text"
-                placeholder="Search by title or author"
-                onChange={this.onSearchQueryChange}
-                value={this.state.query}
-              />
-            </form>
-          </div>
-        </div>
-        <div className="search-books-results">
-          <ol className="books-grid">
-            <li>
-              {this.state.searchResultsFound && (
-                <DisplayBooks
-                  books={this.state.searchResults}
-                  changeBookShelf={(book, shelf) =>
-                    this.props.onBookShelfChange(book, shelf)
-                  }
-                />
-              )}
-            </li>
-          </ol>
-        </div>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="search-books">
+				<div className="search-books-bar">
+					<Link to="/" className="close-search" />
+					<div className="search-books-input-wrapper">
+						<form onChange={this.searchBooks} onSubmit={e => e.preventDefault()}>
+							<DebounceInput
+								type="text"
+								placeholder="Search by title or author"
+								debounceTimeout={300}
+								onChange={this.onSearchQueryChange}
+								value={this.state.query}
+							/>
+						</form>
+					</div>
+				</div>
+				<div className="search-books-results">
+					<ol className="books-grid">
+						<li>
+							{this.state.searchResultsFound &&
+								<DisplayBooks
+									books={this.state.searchResults}
+									changeBookShelf={(book, shelf) => this.props.onBookShelfChange(book, shelf)}
+								/>}
+						</li>
+					</ol>
+					<div>
+						{!this.state.searchResultsFound &&
+							this.state.query &&
+							<Typography align="center" color="primary">
+								No results
+							</Typography>}
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default SearchBooks;
